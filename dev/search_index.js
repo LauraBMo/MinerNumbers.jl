778d9d7d14ad10624f334727fa0e540c93b1b1e5
Documenter.jl <documenter@juliadocs.github.io>
2025-10-09 19:33:25 +0000

    build based on a87630f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MinerNumbers","page":"Home","title":"MinerNumbers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MinerNumbers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
